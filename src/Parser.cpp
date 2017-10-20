b2046419c81d2fe860c40c05e1f3d1aeb6db34f5
micromikko <micromikko@gmail.com>
2017-10-20 13:48:12 +0300

    pikkusäätöä ja parserin alku

---
+++ src/Parser.cpp
@@ -0,0 +1,31 @@
+/*
+ * Parser.cpp
+ *
+ *  Created on: 20.10.2017
+ *      Author: micromikko
+ */
+
+#include "Parser.h"
+
+Parser::Parser() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Parser::~Parser() {
+	// TODO Auto-generated destructor stub
+}
+
+void Parser::generalParser(char *command) {
+
+	switch(command[0]) {
+	case 'G':
+		//gcodeparser
+		break;
+	case 'M':
+		//mcodeparser
+		break;
+	default:
+		//jaska
+	}
+}
