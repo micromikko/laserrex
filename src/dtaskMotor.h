2004471dbefa00fbfccccf9fed50a7ffd174d7e0
micromikko <micromikko@gmail.com>
2017-10-22 00:13:54 +0300

    dtaskMotorin haamu

---
+++ src/dtaskMotor.h
@@ -0,0 +1,13 @@
+/*
+ * dtaskMotor.h
+ *
+ *  Created on: 21.10.2017
+ *      Author: micromikko
+ */
+
+#ifndef DTASKMOTOR_H_
+#define DTASKMOTOR_H_
+
+void motorTask(void *pvParameters);
+
+#endif /* SRC_DTASKMOTOR_H_ */
