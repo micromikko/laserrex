58df7fa530abc4ae9968679531e26721a74989b0
micromikko <micromikko@gmail.com>
2017-10-21 14:15:20 +0300

    testi with compack, parser and laserrex
    asasd

--- src/laserrex.cpp
+++ src/laserrex.cpp
@@ -21,8 +21,6 @@
 #include <string>
 #include <cstring>
 #include <cstdlib>
-#include "uart_module.h"
-
 
 
 // TODO: insert other definitions and declarations here
@@ -49,6 +47,13 @@ static void prvSetupHardware(void) {
 	Board_Init();
 }
 
+
+void dtaskUART(void *pvParameters) {
+
+	for(;;) {
+
+	}
+}
 
 void taskExecute(void *pvParameters) {
 //	Motor xMotor;
@@ -118,13 +123,9 @@ int main(void) {
 	/*
 	 * dtasks
 	 */
-
+	xTaskCreate(dtaskUART, "dtaskUART", 100, NULL, (tskIDLE_PRIORITY + 1UL), NULL);
 	xTaskCreate(dtaskLimit, "dtaskLimit", 100, NULL, (tskIDLE_PRIORITY + 1UL), NULL);
 	xTaskCreate(dtaskButton, "dtaskButton", 100, NULL, (tskIDLE_PRIORITY + 1UL), NULL);
-	xTaskCreate(dtaskUARTReader, 	"dtaskUARTReader", 	256, NULL, (tskIDLE_PRIORITY + 2UL), NULL);
-	xTaskCreate(taskPrinter,		"taskPrinter",		256, NULL, (tskIDLE_PRIORITY + 1UL), NULL);
-
-	UARTModule_init();
 
 
 	vTaskStartScheduler();
