123de27ee477350c05587ea8327847f877981f69
poromise <simo.hyttinen@gmail.com>
2017-10-26 20:32:12 +0300

    hakkaa seiniä vasten. ei käyttistä.
    ei aikaa testata

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -1,5 +1,5 @@
 /*
- * Driver.cpp
+ * Driver.cpp tri trii tri trii tri trii
  *
  *  Created on: 23.10.2017
  *      Author: micromikko
@@ -12,7 +12,8 @@
 #include <cstring>
 #include "FreeRTOS.h"
 #include "task.h"
-//#include "PlotterData.h"
+#include "event_groups.h"
+
 #include "Parser.h"
 #include "Handles.h"
 #include "stdlib.h"
@@ -22,30 +23,19 @@ xSemaphoreHandle sbRIT;
 xSemaphoreHandle motorSemaphore;
 volatile uint32_t RIT_count;
 volatile uint8_t kumpi;
-volatile bool xuunta;
-volatile bool yuunta;
 
 extern "C" {
 	void RIT_IRQHandler(void) {
-		// This used to check if a context switch is required
 		portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
-		// Tell timer that we have processed the interrupt.
-		// Timer then removes the IRQ until next match occurs
-		Chip_RIT_ClearIntStatus(LPC_RITIMER); // clear IRQ flag
+
+		Chip_RIT_ClearIntStatus(LPC_RITIMER);
 		if(RIT_count > 0) {
-//			char asd = RIT_count + '0';
-//			Board_UARTPutSTR("OLLAA IRSSISSA!\r\n");
-//			Board_UARTPutChar(asd);
-//			Board_UARTPutSTR("\r\n");
 			RIT_count--;
-			// do something useful here...
-			xSemaphoreGiveFromISR( motorSemaphore, &xHigherPriorityTaskWoken );
+			xSemaphoreGiveFromISR(motorSemaphore, &xHigherPriorityTaskWoken);
 		} else {
-			Chip_RIT_Disable(LPC_RITIMER); // disable timer
-			// Give semaphore and set context switch flag if a higher priority task was woken up
+			Chip_RIT_Disable(LPC_RITIMER);
 			xSemaphoreGiveFromISR(sbRIT, &xHigherPriorityTaskWoken);
 		}
-		// End the ISR and (possibly) do a context switch
 		portEND_SWITCHING_ISR(xHigherPriorityTaskWoken);
 	}
 }
@@ -96,7 +86,8 @@ void executeCommand(PlotterData *pd, Servo &penServo) {
 				// home
 				break;
 		}
-	} else if(pd->gorm == 'M' && pd->gormNum == 1) {
+	}
+	else if(pd->gorm == 'M' && pd->gormNum == 1) {
 		switch(pd->targetPen) {
 			case 90:
 				penServo.penDown();
@@ -108,77 +99,224 @@ void executeCommand(PlotterData *pd, Servo &penServo) {
 	}
 }
 
+void driveX (bool dir, bool prevDir, DigitalIoPin &dirPin, PlotterData &pd) {
+	if (dir != pd.dirX) {
+		if (dir) {
+			pd.dirX = true;
+		} else {
+			pd.dirX = false;
+		}
+		dirPin.write(pd.dirX);
+	}
+	kumpi = 1;
+	RIT_start(1, 750);
+}
+
+/* Drive Y for one step in the negative direction. Y DRIVE VALUES FLIPPED! */
+void driveY(bool dir, bool prevDir, DigitalIoPin &dirPin, PlotterData &pd) {
+	if (dir != pd.dirY) {
+		if (dir) {
+			pd.dirY = false;
+		} else {
+			pd.dirY = true;
+		}
+		dirPin.write(pd.dirY);
+	}
+	kumpi = 2;
+	RIT_start(1, 750);
+}
+
+
+
+/* Calibration function that is to be run _before_ GPIO interrupts are initialized! */
+void caribourate(PlotterData &pd) {
+
+	/* Initialize pins to use in calibration. Also they need to be in
+	 * pullup mode for use in GPIO interrupts later, and the DigitalIoPin
+	 * constructor also conveniently does that. */
+
+	DigitalIoPin ls1(0, 29, DigitalIoPin::pullup, true);
+	DigitalIoPin ls2(0, 9, DigitalIoPin::pullup, true);
+	DigitalIoPin ls3(1, 3, DigitalIoPin::pullup, true);
+	DigitalIoPin ls4(0, 0, DigitalIoPin::pullup, true);
+
+	DigitalIoPin dirPinX(0, 28, DigitalIoPin::output, false);
+	DigitalIoPin dirPinY(1, 0, DigitalIoPin::output, false);
+
+	/* Pointers for limit switches */
+	DigitalIoPin* x1;
+	DigitalIoPin* x2;
+	DigitalIoPin* y1;
+	DigitalIoPin* y2;
+
+	/* Amount of steps to back up from limit switches. This is calculated in the upcoming loop. */
+	int backupSteps = 500;
+
+	/* Do once per axis */
+	for (int axisNr = 0; axisNr < 2; axisNr++) {
+		/* Drive to zero on each axis */
+		while (!ls1.read() && !ls2.read() && !ls3.read() && !ls4.read()) {
+			if (axisNr == 0) {
+				// X axis
+				driveX(false, pd.dirX, dirPinX, pd);
+			} else {
+				// Y axis
+				driveY(false, pd.dirY, dirPinY, pd);
+			}
+		}
+
+		/* Assign correct switches for the axes */
+		if (ls1.read()) {
+			if (axisNr == 0) {
+				x1 = &ls1;
+				x2 = &ls2;
+			} else {
+				y1 = &ls1;
+				y2 = &ls2;
+			}
+		} else if (ls2.read()) {
+			if (axisNr == 0) {
+				x1 = &ls2;
+				x2 = &ls1;
+			} else {
+				y1 = &ls2;
+				y2 = &ls1;
+			}
+		} else if (ls3.read()) {
+			if (axisNr == 0) {
+				x1 = &ls3;
+				x2 = &ls4;
+			} else {
+				y1 = &ls3;
+				y2 = &ls4;
+			}
+		} else if (ls4.read()) {
+			if (axisNr == 0) {
+				x1 = &ls4;
+				x2 = &ls3;
+			} else {
+				y1 = &ls4;
+				y2 = &ls3;
+			}
+		}
+
+		/* Calculate the amount of steps to back up from the limit switch */
+//		if (backupSteps = 0) {
+//			int stepsTravelled = 0;
+//			while (x1.read() || x2.read()) {
+//				driveX(true, pd.dirX, dirPinX);
+//				stepsTravelled++;
+//			}
+//			backupSteps = (int) ((double) stepsTravelled * (double) 1.5);
+//		}
+		/* Use the calculated amount of backup steps to backup from the Y axis limit switch */
+
+		/* Back up from limit switches */
+		for (int i; i < backupSteps; i++) {
+			if (axisNr == 0) {
+				driveX(false, pd.dirX, dirPinX, pd);
+			} else {
+				driveY(true, pd.dirY, dirPinY, pd);
+			}
+		}
+	}
+	bool xFinished = false;
+	bool yFinished = false;
+	/* Calculate amount of steps to the other end of the axes and travel back to origin. */
+	while (!xFinished || !yFinished) {
+
+		int xStepsTravelled = 0;
+		int yStepsTravelled = 0;
+
+		/* As long as limit switches aren't pressed, drive forward on the X axis */
+		if (!x1->read() && !x2->read()) {
+			driveX(true, pd.dirX, dirPinX, pd);
+			xStepsTravelled++;
+		} else {
+			xFinished = true;
+		}
+
+		/* As long as limit switches aren't pressed, drive forward on the Y axis */
+		if (!y1->read() && !y2->read()) {
+			driveY(true, pd.dirY, dirPinY, pd);
+			yStepsTravelled++;
+		} else {
+			yFinished = true;
+		}
+
+		/* Do this after we are at the positive end of both axes */
+		if (xFinished && yFinished) {
+			/*  */
+			for (int i = 0; i < backupSteps; i++) {
+				driveX(false, pd.dirX, dirPinX, pd);
+				driveY(false, pd.dirY, dirPinY, pd);
+			}
+
+			pd.axisStepCountX = xStepsTravelled - backupSteps;
+			pd.axisStepCountY = yStepsTravelled - backupSteps;
+			// TODO: steps are calculated from X axis info, but the length constants don't match up
+			// to actual measurements (ratio is off) so on the Y axis actual print length will be off
+			pd.stepsPerMM = (double) pd.axisStepCountX / (double) pd.axisLengthX;
+			xStepsTravelled = 0;
+			yStepsTravelled = 0;
+			bool xFinished2 = false;
+			bool yFinished2 = false;
+
+			while(!xFinished2 || !yFinished2) {
+				if (xStepsTravelled < pd.axisStepCountX) {
+					driveX(false, pd.dirX, dirPinX, pd);
+					xStepsTravelled++;
+				}
+				if (yStepsTravelled < pd.axisStepCountY) {
+					driveY(false, pd.dirY, dirPinY, pd);
+					yStepsTravelled++;
+				}
+			}
+
+			pd.absoluteCurrentX = 0.0;
+			pd.absoluteCurrentY = 0.0;
+		}
+	}
+}
+
+
 void taskExecute(void *pvParameters) {
 	Handles *commonHandles = (Handles*) pvParameters;
 	PlotterData *plotdat = new PlotterData(plotdat->pen, 380, 310);
 	Servo penServo(0, 10);
 	Parser parsakaali;
-//	BaseType_t status;
 
+
+
+	NVIC_DisableIRQ(PIN_INT0_IRQn);
+	NVIC_DisableIRQ(PIN_INT1_IRQn);
+	NVIC_DisableIRQ(PIN_INT2_IRQn);
+	NVIC_DisableIRQ(PIN_INT3_IRQn);
+	NVIC_DisableIRQ(PIN_INT4_IRQn);
+	NVIC_DisableIRQ(PIN_INT5_IRQn);
+
+	caribourate(*plotdat);
+
+	NVIC_EnableIRQ(PIN_INT0_IRQn);
+	NVIC_EnableIRQ(PIN_INT1_IRQn);
+	NVIC_EnableIRQ(PIN_INT2_IRQn);
+	NVIC_EnableIRQ(PIN_INT3_IRQn);
+	NVIC_EnableIRQ(PIN_INT4_IRQn);
+	NVIC_EnableIRQ(PIN_INT5_IRQn);
 	// caribourate()
-	// init()
-	/*
-	 * test
-	 */
-//	std::string *rawCommand = new std::string("G1 X0 Y0 A0");
-//	parsakaali.generalParse(plotdat, *rawCommand);
-//	executeCommand(plotdat, penServo);
-//	delete rawCommand;
-
-//	vTaskDelay(100);
-
-//	std::string *com2 = new std::string("G1 X40 Y80 A0");
-//	parsakaali.generalParse(plotdat, *com2);
-//	executeCommand(plotdat, penServo);
-//	delete com2;
-////	vTaskDelay(100);
-//
-//	std::string *com3 = new std::string("G1 X80 Y0 A0");
-//	parsakaali.generalParse(plotdat, *com3);
-//	calculateDrive(plotdat);
-//	delete com3;
-////	vTaskDelay(100);
-//
-//	std::string *com4 = new std::string("G1 X0 Y0 A0");
-//	parsakaali.generalParse(plotdat, *com4);
-//	executeCommand(plotdat, penServo);
-//	delete com4;
 
 	for(;;) {
 		std::string *rawCommand;
 		xQueueReceive(commonHandles->commandQueue_raw, &rawCommand, portMAX_DELAY);
 		parsakaali.generalParse(plotdat, *rawCommand);
 		executeCommand(plotdat, penServo);
-//		vTaskDelay(10);
+//		vTaskDelay(5);
 //		parsakaali.debug(plotdat, *rawCommand, false);
 		delete rawCommand;
 		xSemaphoreGive(commonHandles->readyToReceive);
+
 	}
 }
-
-//void taskExecute(void *pvParameters) {
-//	Handles *commonHandles = (Handles*) pvParameters;
-////	PlotterData plotdat(plotdat.pen, 380, 310);
-//	PlotterData *plotdat = new PlotterData(plotdat->pen, 380, 310);
-//	Parser parsakaali;
-//
-////	plotdat.resetCompack();
-////	parsakaali.debug(plotdat, *rawCommand, false);		// set true or false to see all info in compack or given command
-////	plotdat.resetCompack(); // -.,-.,-.,
-////	RIT_count = 0;
-////	ITM_write("VALMIS\r\n");
-//	for(;;) {
-//
-////		xQueueReceive(commonHandles->commandQueue_raw, &rawCommand, portMAX_DELAY);
-////		parsakaali.generalParse(plotdat, *rawCommand);
-////		calculateDrive(plotdat);
-////		justDrive(plotdat);
-//////		parsakaali.debug(*rawCommand, false);		// set true or false to see all info in compack or given command
-////		plotdat.resetCompack(); // -.,-.,-.,
-////		RIT_count = 0;
-////		xSemaphoreGive(commonHandles->readyToReceive);
-//	}
-//}
 
 void calculateDrive(PlotterData *pd) {
 
@@ -196,15 +334,7 @@ void calculateDrive(PlotterData *pd) {
 	double ratioX = (double) abs(stepDeltaX) / stepDeltaMax;
 	double ratioY = (double) abs(stepDeltaY) / stepDeltaMax;
 
-
-
-
-//	pd->absoluteCurrentX = pd->absoluteTargetX;
-//	pd->absoluteCurrentY = pd->absoluteTargetY;
-//	pd->resetCompack();
 	justDrive(pd, stepAbsoluteCurrentX, stepAbsoluteCurrentY, stepAbsoluteTargetX, stepAbsoluteTargetY, stepDeltaX, stepDeltaY, ratioX, ratioY);
-//	pd->absoluteCurrentX = pd->absoluteTargetX;
-//	pd->absoluteCurrentY = pd->absoluteTargetY;
 }
 
 void justDrive(PlotterData *pd,
@@ -215,59 +345,43 @@ void justDrive(PlotterData *pd,
 
 	DigitalIoPin dirPinX(0, 28, DigitalIoPin::output, false);
 	DigitalIoPin dirPinY(1, 0, DigitalIoPin::output, false);
-//	char commandBuffer[400];
-//	memset(commandBuffer, 0, sizeof(commandBuffer));
-//	const char *format =
-//			"curX: %.2f\r\n"
-//			"curY: %.2f\r\n"
-//			"tarX: %.2f\r\n"
-//			"tarY: %.2f\r\n"
-//
-//			"dA: %d\r\n"
-//			"dB: %d\r\n"
-////			"dMax: %.2f\r\n"
-//			"ratioX: %.2f\r\n"
-//			"ratioY: %.2f\r\n";
-////	memset(commandBuffer, 0, sizeof(commandBuffer));
-//	snprintf(commandBuffer, sizeof(commandBuffer), format,
-//			pd->absoluteCurrentX,
-//			pd->absoluteCurrentY,
-//			pd->absoluteTargetX,
-//			pd->absoluteTargetY,
-//
-//			stepDeltaX,
-//			stepDeltaY,
-////			stepDeltaMax,
-//			ratioX,
-//			ratioY);
-//
-//	ITM_write(commandBuffer);
-	int poro = 500;
+
+	int poro = 400;
 	double countX = 0;
 	double countY = 0;
 
 	bool dirx = true;
 	bool diry = true;
-	dirPinX.write(dirx);
-	dirPinY.write(dirx);
+//	dirPinX.write(dirx);
+//	dirPinY.write(diry);
+	dirPinX.write(pd->dirX);
+	dirPinY.write(pd->dirY);
 
 	while((stepAbsoluteCurrentX != stepAbsoluteTargetX) || (stepAbsoluteCurrentY != stepAbsoluteTargetY)) {
 
 		if(stepAbsoluteCurrentX != stepAbsoluteTargetX){
 			countX += ratioX;
-
 			if(countX >= 1) {
 				if(stepDeltaX > 0) {
 //					xuunta = true;
-					if(dirx != true) {
+//					dirPinX.write(true);
+					if(pd->dirX != true) {
 						dirPinX.write(true);
+						pd->dirX = true;
 					}
+//					if(dirx != true) {
+//						dirPinX.write(true);
+//					}
 					stepAbsoluteCurrentX += 1;
 				} else {
 //					xuunta = false;
-					if(dirx != false) {
+					if(pd->dirX != false) {
 						dirPinX.write(false);
+						pd->dirX = false;
 					}
+//					if(dirx != false) {
+//						dirPinX.write(false);
+//					}
 					stepAbsoluteCurrentX -= 1;
 				}
 
@@ -279,19 +393,26 @@ void justDrive(PlotterData *pd,
 
 		if(stepAbsoluteCurrentY != stepAbsoluteTargetY){
 			countY += ratioY;
-
 			if(countY >= 1) {
 				if(stepDeltaY > 0) {
 //					yuunta = true;
-					if(diry != false) {
+					if(pd->dirY != false) {
 						dirPinY.write(false);
+						pd->dirY = false;
 					}
+//					if(diry != false) {
+//						dirPinY.write(false);
+//					}
 					stepAbsoluteCurrentY += 1;
 				} else {
 //					yuunta = false;
-					if(diry != true) {
+					if(pd->dirY != true) {
 						dirPinY.write(true);
+						pd->dirY = true;
 					}
+//					if(diry != true) {
+//						dirPinY.write(true);
+//					}
 					stepAbsoluteCurrentY -= 1;
 				}
 
@@ -300,20 +421,13 @@ void justDrive(PlotterData *pd,
 				countY -= 1;
 			}
 		}
-		if(poro > 100) {
-			poro -= 2;
+		if(poro > 200) {
+			poro -= 3;
 		}
 	}
 
-
-
-//	if(!((pd.absoluteCurrentX == pd.absoluteTargetX) && (pd.absoluteCurrentY = pd.absoluteTargetY))) {
-//		/*
-//		 * IT'S A TRAP!
-//		 */
-//		while(1);
-//	}
-
+//	pd->dirX = dirx;
+//	pd->dirY = diry;
 	pd->absoluteCurrentX = pd->absoluteTargetX;
 	pd->absoluteCurrentY = pd->absoluteTargetY;
 
@@ -344,6 +458,19 @@ void dtaskMotor(void *pvParameters) {
 			stepPinY.write(false);
 			break;
 		}
+
+//		switch(kumpi) {
+//		case 1:
+////			dirPinX.write(xuunta);
+//			stepPinX.write(true);
+//			stepPinX.write(false);
+//			break;
+//		case 2:
+////			dirPinY.write(!yuunta);
+//			stepPinY.write(true);
+//			stepPinY.write(false);
+//			break;
+//		}
 
 
 
